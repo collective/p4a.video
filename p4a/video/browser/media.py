4093048465396ff77e6d974f654856c8cdd6e5fe
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-12-03 16:26:57 +0000

    Now uses IMediaActivator adapter to change activation status.

--- p4a/video/browser/media.py
+++ p4a/video/browser/media.py
@@ -12,10 +12,10 @@ except ImportError, err:
 
 _marker = object()
 
-class ToggleEnhancementsView(media.MediaActivator):
+class ToggleEnhancementsView(object):
     """
     """
-    
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
@@ -24,14 +24,20 @@ class ToggleEnhancementsView(media.MediaActivator):
         was_activated = self.media_activated
         self.media_activated = not was_activated
         response = self.request.response
-        
+
         if was_activated:
             activated = 'Media+deactivated'
         else:
             activated = 'Media+activated'
-        
+
         response.redirect(self.context.absolute_url() + \
                           '/view?portal_status_message='+activated)
+
+    def _set_media_activated(self, v):
+        interfaces.IMediaActivator(self.context).media_activated = v
+    def _get_media_activated(self):
+        return interfaces.IMediaActivator(self.context).media_activated
+    media_activated = property(_get_media_activated, _set_media_activated)
 
 class BaseMediaDisplayView(form.PageDisplayForm):
     """Base view for displaying media.
