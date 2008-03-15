899a9a1c68b3fc215fc650f9e7fd6ca60afea6f5
nate <nate@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-03-15 19:44:20 +0000

    remove flowplayer since its now in browser

--- p4a/video/flv/_player.py
+++ p4a/video/flv/_player.py
@@ -4,10 +4,12 @@ from p4a.video import interfaces
 from Products.CMFCore import utils as cmfutils
 
 def generate_config(**kw):
-    text = 'config={'
+    text = 'config : {'
     for key, value in kw.items():
-        if value is not None:
+        if value is not None and value is not 'false' and value is not 'true':
             text += "%s: '%s', " % (key, value)
+        else:
+            text += "%s: %s, " % (key, value)            
     if text.endswith(', '):
         text = text[:-2]
     text += ' }'
@@ -25,11 +27,13 @@ class FLVVideoPlayer(object):
         portal_tool = cmfutils.getToolByName(contentobj, 'portal_url')
         portal_url = portal_tool.getPortalObject().absolute_url()
 
-        player = portal_url + "/++resource++flowplayer/FlowPlayer.swf"
-        if not imageurl:
+        player = portal_url + "/++resource++flowplayer/FlowPlayerDark.swf"
+        
+        # TODO: don't use the imageurl until it can return a url with .jpg suffix
+        #if not imageurl:
             # must replace + with %2b so that the FlowPlayer finds the image
-            imageurl = portal_url + \
-                       "/%2b%2bresource%2b%2bflowplayer/play-button-328x240.jpg"
+        imageurl = portal_url + \
+                       "/%2b%2bresource%2b%2bflowplayer/html/play-button-328x240.jpg"
         downloadurl = contentobj.absolute_url()
         title = contentobj.title
 
@@ -42,20 +46,31 @@ class FLVVideoPlayer(object):
         config = generate_config(videoFile=downloadurl,
                                  splashImageFile=imageurl,
                                  autoPlay='false',
+                                 useNativeFullScreen='true',
+                                 initialScale='scale',
                                  loop='false')
 
         return """
-        <div class="flowplayer">
-            <object type="application/x-shockwave-flash" data="%(player)s" 
-            	width="%(width)s" height="%(height)s" id="FlowPlayer">
-            	<param name="allowScriptAccess" value="sameDomain" />
-            	<param name="movie" value="%(player)s" />
-            	<param name="quality" value="high" />
-            	<param name="scale" value="noScale" />
-            	<param name="wmode" value="transparent" />
-            	<param name="flashvars" value="%(config)s" />
-            </object>
+
+        <div id="playerContainer">
         </div>
+
+        <script type="text/javascript">
+        $(document).ready(function() {
+        
+            $("#playerContainer").flashembed({
+                src:'%(player)s',
+                width:%(width)s, 
+                height:%(height)s
+              },
+              { 
+                %(config)s
+              } 
+            );
+
+        });
+        </script>
+
         """ % {'player': player,
                'config': config,
                'title': title,
