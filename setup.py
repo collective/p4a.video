ef5fe4d5ee0d52fe0766cd1fb48f37f1126a3d5f
nate <nate@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-09-14 02:00:30 +0000

    added support for iPod video - video/x-m4v

--- setup.py
+++ setup.py
@@ -8,13 +8,15 @@ setup(name='p4a.video',
       long_description="""p4a.video is a Python video library for dealing
 with video files and their various means of storing/loading metadata.""",
       classifiers=[
+          'Framework :: Zope2',
           'Framework :: Zope3',
+          'Framework :: Plone',
           'Programming Language :: Python',
           'License :: OSI Approved :: GNU General Public License (GPL)',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Multimedia :: Video'
           ],
-      keywords='Plone4Artists',
+      keywords='Plone4Artists video multimedia quicktime flash flv vodcast',
       author='Rocky Burt',
       author_email='rocky@serverzen.com',
       url='http://www.plone4artists.org/products/plone4artistsvideo',
