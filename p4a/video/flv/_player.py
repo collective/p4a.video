ba576e7e62ec9ccd58cf0cdbb4818390488778bb
claytron <claytron@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-07-23 17:00:15 +0000

    merging down the flowplayer3 branch

--- p4a/video/flv/_player.py
+++ p4a/video/flv/_player.py
@@ -3,13 +3,13 @@ from zope import component
 from p4a.video import interfaces
 from Products.CMFCore import utils as cmfutils
 
-def generate_config(**kw):
-    text = 'config : {'
+def generate_config(config_name, **kw):
+    text = '%s : {' % config_name
     for key, value in kw.items():
         if value is not None and value is not 'false' and value is not 'true':
             text += "%s: '%s', " % (key, value)
         else:
-            text += "%s: %s, " % (key, value)            
+            text += "%s: %s, " % (key, value)
     if text.endswith(', '):
         text = text[:-2]
     text += ' }'
@@ -18,68 +18,62 @@ def generate_config(**kw):
 class FLVVideoPlayer(object):
     interface.implements(interfaces.IMediaPlayer)
     component.adapts(object)
-    
+
     def __init__(self, context):
         self.context = context
-    
+
     def __call__(self, downloadurl, imageurl, width, height):
         contentobj = self.context.context.context
         portal_tool = cmfutils.getToolByName(contentobj, 'portal_url')
         portal_url = portal_tool.getPortalObject().absolute_url()
-
-        player = portal_url + "/++resource++flowplayer/FlowPlayerDark.swf"
-        
-        # TODO: don't use the imageurl until it can return a url with .jpg suffix
-        #if not imageurl:
-            # must replace + with %2b so that the FlowPlayer finds the image
-        imageurl = portal_url + \
-                       "/%2b%2bresource%2b%2bflowplayer/html/play-button-328x240.jpg"
+        flow_player_base = portal_url + "/%2B%2Bresource%2B%2Bflowplayer"
+        player = flow_player_base + "/flowplayer-3.2.2.swf"
         downloadurl = contentobj.absolute_url()
-
         title = contentobj.title
-
         if not (width and height):
             width = 320
             height = 240
         # 22 is added to the height so that FlowPlayer controls fit
         height = height + 22
+        image_tag = ""
+        tag = '<img src="%s" alt="%s" style="cursor: pointer; height: %spx; width:%spx" />'
+        image_tag = tag % (imageurl, title, height, width)
+        if not imageurl:
+            image_tag = ""
+        # center the play button
+        buttonTop = height/2 - 22
+        buttonLeft = width/2 - 22
+        config = generate_config(
+            'clip',
+            url=downloadurl,
+            autoPlay='false',
+            )
+        return """\
+<div id="playerContainer"
+     href="%(downloadurl)s"
+     style="display: block; height: %(height)spx; width: %(width)spx; background-color: #000000">
+    <img src="%(flow_player_base)s/example/play.png" alt="Play" class="playButton"
+         style="margin: %(buttonTop)spx 0 0 %(buttonLeft)spx"/>
+    %(image_tag)s
+</div>
 
-        config = generate_config(videoFile=downloadurl,
-                                 splashImageFile=imageurl,
-                                 autoPlay='false',
-                                 useNativeFullScreen='true',
-                                 initialScale='scale',
-                                 videoHeight=height)
-        return """
-            <div id="playerContainer">
-            </div>
-    
-            <script type="text/javascript">
-            $(document).ready(function() {
-            
-                $("#playerContainer").flashembed({
-                    src:'%(player)s',
-                    width:%(width)s, 
-                    height:%(height)s
-                  },
-                  { 
-                    %(config)s
-                  } 
-                );
-    
-            });
-            </script>
-        """ % {'player': player,
-               'config': config,
-               'title': title,
-               'width': width,
-               'height': height}
+<script type="text/javascript">
+jq(document).ready(function() {
 
-        # <div class="hVlog">
-        #   <a href="" class="hVlogTarget" type="" onclick="vPIPPlay(this, '', '', ''); return false;">
-        #       <img src="http://www.plone.org/logo.jpg" /></a>
-        # <br />
-        #   <a href="%(url)s" type="application/x-shockwave-flash" onclick="vPIPPlay(this, 'width=%(width)s, height=%(height)s, name=%(title)s, quality=High, bgcolor=#FFFFFF, revert=true, flv=true', ''FLVbuffer=15', 'active=true, caption=%(title)s'); return false;">
-        # Play Flash version</a>
-        # </div>
-        
+    flowplayer("playerContainer", "%(player)s", {
+        // we need at least this version
+        version: [9, 115],
+
+        // older versions will see a custom message
+        onFail: function()  {
+            document.getElementById("info").innerHTML =
+                "You need the latest Flash version to view MP4 movies. " +
+                "Your version is " + this.getVersion();
+        }
+    }, {
+        // here is our third argument which is the Flowplayer configuration
+        %(config)s
+    });
+});
+</script>
+        """ % locals()
